57d20f96f37f6c80872fda8a7f0697b3340ed2d8
Justinas <justinas@razas.lt>
2024-04-08 13:57:38 +0200

    Created optimizer constructor

--- HeatManagement/Optimizer/Optimizer.cs
+++ HeatManagement/Optimizer/Optimizer.cs
@@ -1,1 +1,8 @@
 namespace HeatManagement;
+
+partial class Optimizer(AssetManager assetmanager, SourceDataManager sourceDataManager, ResultDataManager resultDataManager)
+{
+    private AssetManager Assetmanager = assetmanager;
+    private SourceDataManager SourceDataManager = sourceDataManager;
+    private ResultDataManager ResultDataManager = resultDataManager;
+}
