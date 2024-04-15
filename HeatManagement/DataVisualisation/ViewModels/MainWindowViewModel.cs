998b6fdb4fa77ef3fefd618975634a51563f5ec8
Justinas <justinas@razas.lt>
2024-04-15 15:25:17 +0200

    Data Addition

--- HeatManagement/DataVisualisation/ViewModels/MainWindowViewModel.cs
+++ HeatManagement/DataVisualisation/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
 using Avalonia.Controls;
 namespace HeatManagement.ViewModels;
 
+using System.IO;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 using HeatManagement.Views;
 using ReactiveUI;
 
@@ -17,4 +20,48 @@ class MainWindowViewModel: ViewModelBase{
     public void GoToEditor(){
         CurrentPage = new EditorGreeter();
     }
+
+    public string? ReadFile()
+    {
+        try{
+            var topLevel = App.TopLevel;
+            var files = topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Text File",
+                AllowMultiple = false
+            }).GetAwaiter().GetResult();
+
+            if (files.Count >= 1)
+            {
+                using var stream = files[0].OpenReadAsync().GetAwaiter().GetResult();
+                using var streamReader = new StreamReader(stream);
+                return streamReader.ReadToEnd();
+            }
+        }catch{}
+        return null;
+    }
+
+    AssetManager assetManager = new();
+    SourceDataManager sourceDataManager = new();
+    ResultDataManager resultDataManager = new();
+
+    public void OpenAssetOrSourceFile()
+    {
+        string? fileContent=ReadFile();
+
+        if(fileContent == null){
+            return;
+        }
+
+        try{
+            sourceDataManager.JsonImport(fileContent!);
+        }catch{
+            try{
+                assetManager.JsonImport(fileContent!);
+            }catch{
+                return;
+            }
+        }
+    }
+
 }
